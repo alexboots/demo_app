3ebc2c8bc1b347293095b5f61792a34c1d12c792
Alex Hays <alexhays@gmail.com>
2012-04-24 23:38:09 -0500

    big enough to beat the giant and impeach the tyrant

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'c0a9ed6ce9d0093a92531d0daf48c307f815fde05707b89cec1db393c1779cad47058a101d57f0701fab6c7f080039fa410173df53456c765c29e36919b9f0ed'
